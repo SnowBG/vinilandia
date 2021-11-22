420eea15673ffcba798f1bcd9134d64d59a4b081
snowbg <snowbg@gmail.com>
2021-11-22 08:24:44 -0300

    Migrations made for Question and Choice fields.

--- polls/models.py
+++ polls/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_lenght=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
 class Choice(models.Model):
-    question = models.ForeingKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
